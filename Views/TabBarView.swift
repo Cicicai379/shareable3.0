82fde40006a6da3405dc8738605747ad59fdfde4
Cicicai379 <103560915+Cicicai379@users.noreply.github.com>
2024-01-16 15:35:17 +0800

    hi

---
+++ Views/TabBarView.swift
@@ -0,0 +1,47 @@
+//
+//  TabBarView.swift
+//  SwiftUIStarterKitApp
+//
+//  Created by Osama Naeem on 02/08/2019.
+//  Copyright © 2019 NexThings. All rights reserved.
+//
+
+import SwiftUI
+
+struct TabbarView: View {
+    var body: some View {
+        TabView {
+            NavigationView {
+                ActivitiesContentView(activtiesData: Activities(data: ActivitiesMockStore.activityData, items: ActivitiesMockStore.activities))
+            }
+            .tag(0)
+            .tabItem {
+                Image(systemName: "house")
+                    .resizable()
+                Text("Home")
+            }
+            
+
+            NavigationView {
+                ActivitiesCartView(ShoppingCartItemsData: ActivitiesCart(data: ActivitiesMockStore.shoppingCartData))
+            }
+            .tag(1)
+            .tabItem {
+                Image(systemName:"bag")
+                Text("Items")
+            }
+            
+            NavigationView {
+                     AccountView()
+                  }
+                   .tag(2)
+                    .tabItem {
+                        Image(systemName:"person")
+                    Text("Profile")
+                }
+        }
+    }
+}
+
+
+
